a959c1fae4546b1a5b4e73948336cb18ee22513c
Miska Kananen <miska.kananen@gmail.com>
2023-12-27 22:31:51 +0200

    Open a TCP stream between client and server

--- src/bin/luola-client/main.rs
+++ src/bin/luola-client/main.rs
@@ -1,3 +1,15 @@
+use std::net::TcpStream;
+
+fn open_stream() -> TcpStream {
+    let addr = "127.0.0.1:26988";
+    let stream = match TcpStream::connect(addr) {
+        Ok(s) => s,
+        Err(e) => panic!("failed to connect to {}: {:?}", addr, e),
+    };
+
+    stream
+}
+
 fn main() {
-    println!("client");
+    open_stream();
 }
--- src/bin/luola-server/main.rs
+++ src/bin/luola-server/main.rs
@@ -1,3 +1,31 @@
+use std::net::{TcpListener, TcpStream};
+
+fn wait_for_connections(expected_players: usize) -> Vec<TcpStream> {
+    let addr = "127.0.0.1:26988";
+
+    let listener = match TcpListener::bind(addr) {
+        Ok(l) => l,
+        Err(e) => panic!("failed to bind to address {}: {:?}", addr, e),
+    };
+
+    let mut sockets: Vec<TcpStream> = Vec::new();
+
+    while sockets.len() < expected_players {
+        match listener.accept() {
+            Ok((socket, addr)) => {
+                println!("new connection from {}", addr);
+                sockets.push(socket);
+            }
+            Err(e) => println!("failed to accept connection: {:?}", e),
+        }
+    }
+
+    sockets
+}
+
 fn main() {
-    println!("server");
+    let player_count: usize = 1;
+
+    let sockets = wait_for_connections(player_count);
+    println!("{}", sockets.len());
 }
