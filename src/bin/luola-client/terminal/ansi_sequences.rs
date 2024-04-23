51915281b67ed1dbf04f9c22693cfb3511aa4124
Miska Kananen <miska.kananen@gmail.com>
2024-04-23 19:14:58 +0300

    Add support for bright colors

--- src/bin/luola-client/terminal/ansi_sequences.rs
+++ src/bin/luola-client/terminal/ansi_sequences.rs
@@ -1,4 +1,4 @@
-use crate::terminal::color::Color;
+use crate::terminal::color::{self, Color};
 
 fn prefix() -> String {
     String::from("\x1b[")
@@ -36,17 +36,51 @@ fn color_str(color: Color) -> String {
         Color::Magenta => String::from("5"),
         Color::Cyan => String::from("6"),
         Color::White => String::from("7"),
+        Color::BrightBlack => String::from("0"),
+        Color::BrightRed => String::from("1"),
+        Color::BrightGreen => String::from("2"),
+        Color::BrightYellow => String::from("3"),
+        Color::BrightBlue => String::from("4"),
+        Color::BrightMagenta => String::from("5"),
+        Color::BrightCyan => String::from("6"),
+        Color::BrightWhite => String::from("7"),
         Color::RGB(r, g, b) => format!("8;2;{};{};{}", r, g, b),
         Color::Transparent => panic!("must have a color"),
     }
 }
 
 pub fn set_foreground_color(color: Color) -> String {
+    if color::is_bright(color) {
+        return set_bright_foreground_color(color);
+    }
+
+    set_nonbright_foreground_color(color)
+}
+
+pub fn set_background_color(color: Color) -> String {
+    if color::is_bright(color) {
+        return set_bright_background_color(color);
+    }
+
+    set_nonbright_background_color(color)
+}
+
+fn set_nonbright_foreground_color(color: Color) -> String {
     let sgr = format!("3{}", color_str(color));
     format_text(sgr)
 }
 
-pub fn set_background_color(color: Color) -> String {
+fn set_nonbright_background_color(color: Color) -> String {
     let sgr = format!("4{}", color_str(color));
+    format_text(sgr)
+}
+
+fn set_bright_foreground_color(color: Color) -> String {
+    let sgr = format!("9{}", color_str(color));
+    format_text(sgr)
+}
+
+fn set_bright_background_color(color: Color) -> String {
+    let sgr = format!("10{}", color_str(color));
     format_text(sgr)
 }
--- src/bin/luola-client/terminal/color.rs
+++ src/bin/luola-client/terminal/color.rs
@@ -8,6 +8,25 @@ pub enum Color {
     Magenta,
     Cyan,
     White,
+    BrightBlack,
+    BrightRed,
+    BrightGreen,
+    BrightYellow,
+    BrightBlue,
+    BrightMagenta,
+    BrightCyan,
+    BrightWhite,
     RGB(u8, u8, u8),
     Transparent,
+}
+
+pub fn is_bright(color: Color) -> bool {
+    color == Color::BrightBlack
+        || color == Color::BrightRed
+        || color == Color::BrightGreen
+        || color == Color::BrightYellow
+        || color == Color::BrightBlue
+        || color == Color::BrightMagenta
+        || color == Color::BrightCyan
+        || color == Color::BrightWhite
 }
